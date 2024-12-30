16bfa6b9ec71415004b210f773f87207131e6b7c
NABA RATAN PATRA <58278429+nabaratanpatra@users.noreply.github.com>
2024-12-31 01:37:11 +0530

    testing

--- src/Hooks/UseTitle.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useEffect } from 'react';
-const UseTitle = (title) => {
-    useEffect(()=>{
-        document.title= `${title}- CleanerGuy`
-    }, [title])
-};
-export default UseTitle;
